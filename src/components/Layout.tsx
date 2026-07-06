6c35e9e60d1d95c06044c3e7c94186bad181b51f
ray2005610-gif <ray2005610@gmail.com>
2026-07-06 09:22:45 +0800

    測試

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,6 +8,7 @@ export function Layout({ children }: { children: ReactNode }) {
           <h1>BeadFlow</h1>
           <p>拼豆圖紙辨識、繪製、製作輔助與庫存管理</p>
         </div>
+        <span className="version-badge">測試版本 2026-07-06 09:19</span>
       </header>
       {children}
     </div>
